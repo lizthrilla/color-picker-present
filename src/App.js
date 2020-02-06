01eb94340f1dd63616854b97d0b3efb4d2f7e154
Liz Tiller <elizabeth.tiller@gmail.com>
2020-02-06 16:00:25 -0500

    practice-3 finished

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useEffect, useReducer } from "react";
 import "./App.css";
 import Slider from "./components/Slider";
 
+const initialState = {
+  hue: 250,
+  sat: 100,
+  light: 50
+};
+
+const colorReducer = (state, action) => {
+  switch (action.type) {
+    case 'HUE':
+      return {...state, hue: action.payload}
+    case 'SAT':
+      return { ...state, sat: action.payload }
+    case 'LIGHT':
+      return {...state, light: action.payload}
+    default:
+      return {state}
+  }
+}
+
 function App() {
-  const hsl = `hsla(250, 100%, 50%)`
+  const [state, dispatch] = useReducer(colorReducer, initialState)
+
+  useEffect(() => {
+    const randomHue = () => {
+      const payload = Math.ceil(Math.random() * 330);
+      dispatch({ type: 'HUE', payload})
+    }
+    randomHue()
+  }, []);
+
+  const hsl = `hsla(${state.hue}, ${state.sat}%, ${state.light}%)`
   console.log(hsl)
 
-  const onSliderChange = (e) => {
-    console.log(e.target.value)
+  const onSliderChange = (e, type) => {
+    const value = e.target.value
+    dispatch({type, payload: value})
   }
 
   return (
@@ -15,22 +45,22 @@ function App() {
       <header className="App-header">
         <div className="swatch" style={{ backgroundColor: hsl }} />
         <Slider
-          onChange={e => onSliderChange(e)}
+          onChange={e => onSliderChange(e, 'HUE')}
           sliderMax="330"
-          sliderValue={250}
+          sliderValue={state.hue}
           sliderLabel={"Hue"}
         />
         <Slider
-          onChange={e => onSliderChange(e)}
+          onChange={e => onSliderChange(e, 'SAT')}
           sliderLabel={"Saturation"}
           sliderMax="100"
-          sliderValue={100}
+          sliderValue={state.sat}
         />
         <Slider
-          onChange={e => onSliderChange(e)}
+          onChange={e => onSliderChange(e, 'LIGHT')}
           sliderLabel={"Light"}
           sliderMax="100"
-          sliderValue={50}
+          sliderValue={state.light}
         />
       </header>
     </div>
