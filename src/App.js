1cbaffa08ea1bbd2825706643be61a72749b2f6a
Liz Tiller <elizabeth.tiller@gmail.com>
2020-02-06 14:34:09 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,42 +1,63 @@
-import React, { useState } from "react";
+import React, { useEffect, useReducer } from "react";
 import "./App.css";
 import Slider from "./components/Slider";
 
-function App() {
-  const [hue, setHue] = useState(250)
+const initialState = {
+  hue: 250,
+  sat: 100,
+  light: 50
+};
 
-  // const hsla = `hsla(${hue}, ${sat}%, ${light}%, ${alpha})`;
-  const hsla = `hsla(${hue}, 100%, 50%, 1)`
-  console.log(hsla)
-  console.log(hue)
+const colorReducer = (state, action) => {
+  switch (action.type) {
+    case "HUE":
+      return { ...state, hue: action.payload };
+    case "SAT":
+      return { ...state, sat: action.payload };
+    case "LIGHT":
+      return { ...state, light: action.payload };
+    default:
+      return { state };
+  }
+};
+
+function App() {
+  const [state, dispatch] = useReducer(colorReducer, initialState);
+
+  useEffect(() => {
+    const randomColor = Math.ceil(Math.random() * 330);
+
+    dispatch({ type: "HUE", payload: randomColor });
+  }, []);
+
+  const hsl = `hsl(${state.hue}, ${state.sat}%, ${state.light}%)`;
+
+  const onSliderInputChange = (e, type) => {
+    const value = e.target.value;
+    dispatch({ type: type, payload: value });
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <div className="swatch" style={{ backgroundColor: hsla }} />
+        <div className="swatch" style={{ backgroundColor: hsl }} />
         <Slider
-          onChange={e => console.log(e, "HUE")}
-          sliderMax="360"
-          sliderValue={hue}
+          onChange={e => onSliderInputChange(e, "HUE")}
+          sliderMax="330"
+          sliderValue={state.hue}
           sliderLabel={"Hue"}
         />
         <Slider
-          onChange={e => console.log(e, "SAT")}
+          onChange={e => onSliderInputChange(e, "SAT")}
           sliderLabel={"Saturation"}
           sliderMax="100"
-          sliderValue={100}
+          sliderValue={state.sat}
         />
         <Slider
-          onChange={e => console.log(e, "LIGHT")}
+          onChange={e => onSliderInputChange(e, "LIGHT")}
           sliderLabel={"Light"}
           sliderMax="100"
-          sliderValue={50}
-        />
-        <Slider
-          onChange={e => console.log(e, "ALPHA")}
-          sliderLabel={"Alpha"}
-          sliderMax="1"
-          sliderValue={1}
+          sliderValue={state.light}
         />
       </header>
     </div>
